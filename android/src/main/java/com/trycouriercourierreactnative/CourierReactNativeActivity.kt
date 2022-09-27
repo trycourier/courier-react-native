5bc8ccc1b99bb0bea8b1a6c7471934fdc894f252
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-09-28 04:17:23 +0600

    feat(courier): add askpermission function to courierReactNativeActivity

--- android/src/main/java/com/trycouriercourierreactnative/CourierReactNativeActivity.kt
+++ android/src/main/java/com/trycouriercourierreactnative/CourierReactNativeActivity.kt
@@ -2,7 +2,9 @@ package com.trycouriercourierreactnative
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.courier.android.Courier
+import com.courier.android.requestNotificationPermission
 import com.courier.android.trackPushNotificationClick
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -12,7 +14,7 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.modules.core.DeviceEventManagerModule
 import com.google.firebase.messaging.RemoteMessage
 
-public open class CourierReactNativeActivity: ReactActivity() {
+public open class CourierReactNativeActivity : ReactActivity() {
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -44,20 +46,43 @@ public open class CourierReactNativeActivity: ReactActivity() {
 
   private fun sendEvent(eventName: String, params: WritableMap) {
     val reactContext = getReactInstanceManager().getCurrentReactContext();
-    println("rctContext "+ reactContext);
-    if(reactContext != null){
+    println("rctContext " + reactContext);
+    if (reactContext != null) {
       reactContext
         .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
         .emit(eventName, params)
     }
   }
 
+  private fun setup() {
+    requestNotificationPermission { isGranted ->
+      if (isGranted) {
+        println("***************Notification permission granted ***********");
+//        Toast.makeText(
+//          this@CourierReactNativeActivity,
+//          "Notification permissions are granted",
+//          Toast.LENGTH_LONG
+//        ).show()
+        // Init Courier
+        Courier.initialize(context = this)
+//        Toast.makeText(this, "SDK Initialized", Toast.LENGTH_LONG).show()
+      } else {
+//        Toast.makeText(
+//          this@CourierReactNativeActivity,
+//          "Notification permissions not granted",
+//          Toast.LENGTH_LONG
+//        ).show()
+
+        println("***************Notification permission not granted ***********");
+        setup()
+      }
+    }
+  }
 
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    Courier.initialize(this)
+    setup()
 
     // See if there is a pending click event
     checkIntentForPushNotificationClick(intent)
