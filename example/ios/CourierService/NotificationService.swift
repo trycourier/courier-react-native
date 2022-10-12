de5ce00c1161f0b2483c56d273cfae56d70b9d5a
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-10-12 22:36:36 +0600

    chore(courier-ios) :construction: add CourierService

---
+++ example/ios/CourierService/NotificationService.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationService.swift
+//  CourierService
+//
+//  Created by Fahad Amin on 10/12/22.
+//
+
+import Courier
+
+class NotificationService: CourierNotificationServiceExtension {
+
+    //
+    //         ^      ╔══════════════════════════════╗
+    //       >' )     ║ You can override this class, ║
+    //       ( ( \   <  but it is not recommended    ║
+    //      mm''|\    ╚══════════════════════════════╝
+    //
+
+}
