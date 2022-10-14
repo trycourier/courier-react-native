22f7890557cde21ce9f8561f4d7340195bc63d55
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-10-15 00:52:32 +0600

    chore(courier): add comments to exposed functios

--- src/index.ts
+++ src/index.ts
@@ -36,18 +36,53 @@ type SignInProps = {
   accessToken: String;
   userId: string;
 };
+
+/**
+ * signs user in, persists signin in between sessions
+ * @example
+ * ```
+ *    const res = await signIn({
+        accessToken: ACCESS_TOKEN,
+        userId: USER_ID,
+      });
+ * ```
+ * @param param0 Object{accessToken, userId}
+ * @returns promise
+ */
 export function signIn({ accessToken, userId }: SignInProps): Promise<string> {
   return CourierReactNative.signIn(userId, accessToken);
 }
 
+/**
+ * @example
+ * ```
+ * const fcmToken = await getFcmToken();
+ * ```
+ * @returns Promise with string or undefined depending on available fcm token
+ */
 export function getFcmToken(): Promise<string | undefined> {
   return CourierReactNative.getFcmToken();
 }
 
+/**
+ * @example
+ * ```
+ * const userId = await getUserId();
+ * ```
+ * @returns Promise with string or undefined depending on available userId
+ */
 export function getUserId(): Promise<string | undefined> {
   return CourierReactNative.getUserId();
 }
 
+/**
+ * loggs user out and remove all stored user credentials
+ * @example
+ * ```
+ *   const res = await signOut();
+ * ```
+ * @returns Promise
+ */
 export function signOut(): Promise<string> {
   return CourierReactNative.signOut();
 }
@@ -60,6 +95,26 @@ type SendPushProps = {
   providers: (CourierProvider.FCM | CourierProvider.APNS)[];
   isProduction: boolean;
 };
+
+/**
+ * can be used to test pus notification while development
+ * @example
+ * ```
+ *      const res = await sendPush({
+        authKey: ACCESS_TOKEN,
+        userId: USER_ID,
+        title: 'This is a title',
+        body: 'This is a body',
+        providers: [
+          Platform.OS === 'ios'
+            ? CourierProvider.APNS
+            : CourierProvider.FCM,
+        ],
+        isProduction: !__DEV__,
+      });
+ * ```
+ * @returns promise
+ */
 export function sendPush({
   authKey,
   userId,
@@ -78,6 +133,23 @@ export function sendPush({
   );
 }
 
+/**
+ * @example 
+ *```
+	const addNotificationListeners = () =>
+  registerPushNotificationListeners<NotificationType>({
+    onNotificationClicked: (notification) => {
+				...
+    },
+    onNotificationDelivered: (notification) => {
+				...
+    },
+  });
+	const unsubscribeAddNotificationListener = addNotificationListeners();
+	unsubscribeAddNotificationListener()
+ *```
+ * @returns  function that can be used to unsubscribe from registered listeners
+ */
 export function registerPushNotificationListeners<T>({
   onNotificationClicked,
   onNotificationDelivered,
@@ -115,6 +187,16 @@ export function registerPushNotificationListeners<T>({
   };
 }
 
+/**
+ * subscribes to native courier logs
+ * @example
+ * ```
+ * const unsubscribeDebugListener = debuggerListener();
+ * unsubscribeDebugListener
+ * ```
+ * @returns function which can be used to unsubscribe from debug log
+ *
+ */
 export function debuggerListener() {
   const eventListener = courierEventEmitter.addListener(
     'courierDebugEvent',
@@ -125,6 +207,14 @@ export function debuggerListener() {
   return eventListener.remove;
 }
 
+/**
+ * can be used to listen to native debug logs
+ * @example
+ * ```
+ *setDebugMode(true);
+ * ```
+ * @returns promise with isDebugging status
+ */
 export async function setDebugMode(isDebugging: boolean) {
   return {
     status: (await CourierReactNative.setDebugMode(
@@ -133,23 +223,49 @@ export async function setDebugMode(isDebugging: boolean) {
   };
 }
 
+/**
+ * @example await requestNotificationPermission
+ * requests notification permission
+ * can be called anywhere in the app
+ * @returns promise with notification permission status
+ */
 export function requestNotificationPermission(): Promise<string> {
   return CourierReactNative.requestNotificationPermission();
 }
 
+/**
+ * @example const notificationStatus = await getNotificationPermissionStatus()
+ * @returns promise with notification status
+ */
 export function getNotificationPermissionStatus(): Promise<string> {
   return CourierReactNative.getNotificationPermissionStatus();
 }
 
-export function iOSForegroundPresentationOptions(params: {
+/**
+ * @example iOSForegroundPresentationOptions({options: ['sound']})
+ * takes an array of strings and sets foreground notification for ios
+ * @param {} {options:['sound', 'badge' , 'list' ,'banner']}
+ */
+export function iOSForegroundPresentationOptions({
+  options,
+}: {
   options: ('sound' | 'badge' | 'list' | 'banner')[];
-}) {
-  const normalizedParams = new Set(params.options);
-  return CourierReactNative.iOSForegroundPresentationOptions({
-    options: Array.from(normalizedParams),
-  });
+}): void {
+  if (Platform.OS === 'ios') {
+    const normalizedParams = new Set(options);
+    return CourierReactNative.iOSForegroundPresentationOptions({
+      options: Array.from(normalizedParams),
+    });
+  }
 }
 
+/**
+ * @example
+ * ```
+ * const currentApnsToken = await getApnsToken();
+ * ```
+ * @returns returns promise with string or undefined for ios
+ */
 export function getApnsToken(): Promise<string | undefined> {
   if (Platform.OS === 'android') {
     return Promise.reject(
@@ -159,6 +275,12 @@ export function getApnsToken(): Promise<string | undefined> {
   return CourierReactNative.getApnsToken();
 }
 
+/**
+ * @example
+ * await setFcmToken('token')
+ * sets fcm token for both ios and android
+ * @returns returns promise with string.
+ */
 export function setFcmToken(token: string): Promise<string> {
   return CourierReactNative.setFcmToken(token);
 }
