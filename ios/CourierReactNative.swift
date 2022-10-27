b4261b1cc18c08096d44db18bd2c48ee3946c228
mikemilla <mike@mikemiller.design>
2022-10-27 14:40:36 -0400

    chore: initial commit

---
+++ ios/CourierReactNative.swift
@@ -0,0 +1,8 @@
+@objc(CourierReactNative)
+class CourierReactNative: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
