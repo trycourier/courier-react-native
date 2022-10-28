666cb0435304c96a737855c8e5e225b700b5cf77
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-10-29 00:10:29 +0600

    chore(courier-android): refactor CourierReactNativeActivity to only store courier functionalities

--- android/src/main/java/com/courierreactnative/CourierReactNativeActivity.kt
+++ android/src/main/java/com/courierreactnative/CourierReactNativeActivity.kt
@@ -6,48 +6,15 @@ import com.courier.android.Courier
 import com.courier.android.pushNotification
 import com.courier.android.trackPushNotificationClick
 import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.ReactRootView
 import com.facebook.react.modules.core.DeviceEventManagerModule
 import com.google.firebase.messaging.RemoteMessage
 import org.json.JSONObject
 
-open class CourierReactNativeActivity(private val isNewArchitectureEnabled: Boolean = false) : ReactActivity() {
-
+open class CourierReactNativeActivity() : ReactActivity() {
   companion object {
-    private const val COMPONENT_NAME = "main"
     internal const val PUSH_CLICKED_EVENT = "pushNotificationClicked"
     internal const val PUSH_DELIVERED_EVENT = "pushNotificationDelivered"
   }
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String? {
-    return COMPONENT_NAME
-  }
-
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. There the RootView is created and
-   * you can specify the rendered you wish to use (Fabric or the older renderer).
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate {
-    return MainActivityDelegate(this, mainComponentName, isNewArchitectureEnabled)
-  }
-
-  class MainActivityDelegate(activity: ReactActivity?, mainComponentName: String?, private val isNewArchitectureEnabled: Boolean) : ReactActivityDelegate(activity, mainComponentName) {
-
-    override fun createRootView(): ReactRootView {
-      return ReactRootView(context).apply {
-
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        setIsFabric(isNewArchitectureEnabled)
-      }
-    }
-
-  }
-
   private fun sendEvent(eventName: String, params: String) {
     val reactContext = reactInstanceManager.currentReactContext
     reactContext?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)?.emit(eventName, params)
