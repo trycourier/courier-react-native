0c2e04b3e82b0b65a73b7c39446a30ab0eb4790b
mikemilla <mike@mikemiller.design>
2023-08-29 16:16:49 -0700

    Fetching and pagination

---
+++ MySwiftClass.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  courier-react-native
+//
+//  Created by Michael Miller on 8/29/23.
+//
+
+import Foundation
+
+class Test {
+    
+    func something() {
+        print("OK")
+    }
+    
+}
