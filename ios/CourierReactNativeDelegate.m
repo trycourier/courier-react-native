8276af57d38043ebeba62a3d4df8208ec36d087a
mikemilla <mike@mikemiller.design>
2024-11-14 13:35:06 -0800

    🚀 5.0.2

--- ios/CourierReactNativeDelegate.m
+++ ios/CourierReactNativeDelegate.m
@@ -22,7 +22,7 @@ NSUInteger notificationPresentationOptions = UNNotificationPresentationOptionNon
     if (self) {
         
         // Set the user agent
-        Courier.agent = [CourierAgent reactNativeIOS:@"5.0.1"];
+        Courier.agent = [CourierAgent reactNativeIOS:@"5.0.2"];
         
         // Register for remote notifications
         UIApplication *app = [UIApplication sharedApplication];
