495d43b3254934cc315ee308d83fe13c400fee9d
Mike Miller <mike@mikemiller.design>
2025-04-23 14:32:13 -0600

    🚀 5.5.6

--- ios/CourierReactNativeDelegate.m
+++ ios/CourierReactNativeDelegate.m
@@ -7,12 +7,18 @@
 
 @import Courier_iOS;
 #import "CourierReactNativeDelegate.h"
+#import <React/RCTBridge.h>
+#import <React/RCTBridge+Private.h>
+#import <React/RCTUtils.h>
+
 #pragma GCC diagnostic ignored "-Wprotocol"
 #pragma clang diagnostic ignored "-Wprotocol"
 
 @interface CourierReactNativeDelegate ()
 
 @property (nonatomic, assign) UNNotificationPresentationOptions notificationPresentationOptions;
+@property (nonatomic, strong) NSDictionary *pendingNotificationClick;
+@property (nonatomic, assign) BOOL isReactNativeReady;
 
 @end
 
@@ -25,86 +31,56 @@ static NSString *const CourierForegroundOptionsDidChangeNotification = @"iosFore
     
     if (self) {
         // Set the user agent
-        Courier.agent = [CourierAgent reactNativeIOS:@"5.5.5"];
+        Courier.agent = [CourierAgent reactNativeIOS:@"5.5.6"];
         
         // Register for remote notifications
         UIApplication *app = [UIApplication sharedApplication];
         [app registerForRemoteNotifications];
         
-        // Register notification center changes
+        // Set notification center delegate
         UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
         center.delegate = self;
         
+        // Detect JS bridge load
+        self.isReactNativeReady = [self detectIfBridgeIsReady];
+
+        [[NSNotificationCenter defaultCenter]
+            addObserver:self
+            selector:@selector(onJavaScriptDidLoad:)
+            name:RCTJavaScriptDidLoadNotification
+            object:nil];
+
         [[NSNotificationCenter defaultCenter]
             addObserver:self
             selector:@selector(notificationPresentationOptionsUpdate:)
             name:CourierForegroundOptionsDidChangeNotification
-            object:nil
-        ];
+            object:nil];
     }
     
     return self;
 }
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    // Killed-state launch support
-    [self handleNotificationLaunchFromKilledState:launchOptions];
-
-    return YES;
+- (BOOL)detectIfBridgeIsReady {
+    // RCTBridge currentBridge is non-nil only after JS is loaded
+    return [RCTBridge currentBridge] != nil;
 }
 
 - (void)notificationPresentationOptionsUpdate:(NSNotification *)notification
 {
-    if ([[notification name] isEqualToString:CourierForegroundOptionsDidChangeNotification])
-    {
-        NSDictionary *userInfo = notification.userInfo;
-        self.notificationPresentationOptions = ((NSNumber *)[userInfo objectForKey:@"options"]).unsignedIntegerValue;
-    }
+    NSDictionary *userInfo = notification.userInfo;
+    self.notificationPresentationOptions = ((NSNumber *)[userInfo objectForKey:@"options"]).unsignedIntegerValue;
 }
 
-/// Handles notification taps when the app launches from a killed state.
-/// Call this from AppDelegate's didFinishLaunchingWithOptions using UIApplicationLaunchOptionsRemoteNotificationKey.
-- (void)handleNotificationLaunchFromKilledState:(NSDictionary *)launchOptions
+- (void)onJavaScriptDidLoad:(NSNotification *)notification
 {
-    NSDictionary *userInfo = launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey];
-    if (userInfo) {
-        // Track event
-        [userInfo trackMessageWithEvent:CourierTrackingEventClicked completion:^(NSError * _Nullable error) {
-            if (error) {
-                NSLog(@"[Courier] Failed to track message click on cold start: %@", error.localizedDescription);
-            }
-        }];
+    self.isReactNativeReady = YES;
 
-        // Create temporary content for formatting
-        UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
-        content.userInfo = userInfo;
-
-        dispatch_async(dispatch_get_main_queue(), ^{
-            NSDictionary *formatted = [Courier formatPushNotificationWithContent:content];
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotificationClicked" object:nil userInfo:formatted];
-        });
+    if (self.pendingNotificationClick) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotificationClicked"
+                                                            object:nil
+                                                          userInfo:self.pendingNotificationClick];
+        self.pendingNotificationClick = nil;
     }
-}
-
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-       willPresentNotification:(UNNotification *)notification
-         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
-{
-    UNNotificationContent *content = notification.request.content;
-    NSDictionary *message = content.userInfo;
-    
-    [message trackMessageWithEvent:CourierTrackingEventDelivered completion:^(NSError * _Nullable error) {
-        if (error) {
-            NSLog(@"Error tracking message: %@", error.localizedDescription);
-        }
-    }];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSDictionary *pushNotification = [Courier formatPushNotificationWithContent:content];
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotificationDelivered" object:nil userInfo:pushNotification];
-        completionHandler(self.notificationPresentationOptions);
-    });
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
@@ -113,24 +89,53 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 {
     UNNotificationContent *content = response.notification.request.content;
     NSDictionary *message = content.userInfo;
-    
+
     [message trackMessageWithEvent:CourierTrackingEventClicked completion:^(NSError * _Nullable error) {
         if (error) {
-            NSLog(@"Error tracking message: %@", error.localizedDescription);
+            NSLog(@"[Courier] Error tracking message: %@", error.localizedDescription);
+        }
+    }];
+
+    NSDictionary *pushNotification = [Courier formatPushNotificationWithContent:content];
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (self.isReactNativeReady) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotificationClicked"
+                                                                object:nil
+                                                              userInfo:pushNotification];
+        } else {
+            self.pendingNotificationClick = pushNotification;
+        }
+        completionHandler();
+    });
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+{
+    UNNotificationContent *content = notification.request.content;
+    NSDictionary *message = content.userInfo;
+
+    [message trackMessageWithEvent:CourierTrackingEventDelivered completion:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"[Courier] Error tracking delivery: %@", error.localizedDescription);
         }
     }];
 
     dispatch_async(dispatch_get_main_queue(), ^{
         NSDictionary *pushNotification = [Courier formatPushNotificationWithContent:content];
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotificationClicked" object:nil userInfo:pushNotification];
-        completionHandler();
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotificationDelivered"
+                                                            object:nil
+                                                          userInfo:pushNotification];
+        completionHandler(self.notificationPresentationOptions);
     });
 }
 
 - (void)application:(UIApplication *)application
 didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
-    NSLog(@"Failed to register for remote notification token: %@", error.localizedDescription);
+    NSLog(@"[Courier] Failed to register for remote notifications: %@", error.localizedDescription);
 }
 
 - (void)application:(UIApplication *)application
