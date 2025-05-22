8288e53e37121e7e39918a1f5145cbeec593d503
Mike Miller <mike@mikemiller.design>
2025-05-22 14:11:06 -0500

    🚀 5.6.0

--- android/src/main/java/com/courierreactnative/Utils.kt
+++ android/src/main/java/com/courierreactnative/Utils.kt
@@ -15,7 +15,7 @@ import com.facebook.react.modules.core.DeviceEventManagerModule
 import com.google.gson.GsonBuilder
 
 internal object Utils {
-  val COURIER_AGENT = CourierAgent.ReactNativeAndroid(version = "5.5.9")
+  val COURIER_AGENT = CourierAgent.ReactNativeAndroid(version = "5.6.0")
 }
 
 internal fun ReactContext.sendEvent(eventName: String, value: Any?) {
