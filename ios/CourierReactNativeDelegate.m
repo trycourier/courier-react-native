cf7ff2dc240d8fd7b789720ded632d1d90137e40
Mike Miller <mike@mikemiller.design>
2025-07-22 09:40:41 -0700

    🚀 5.6.11

--- ios/CourierReactNativeDelegate.m
+++ ios/CourierReactNativeDelegate.m
@@ -33,7 +33,7 @@ static NSString *const CourierForegroundOptionsDidChangeNotification = @"iosFore
     if (self) {
       
         // Set the user agent
-        Courier.agent = [CourierAgent reactNativeIOS:@"5.6.10"];
+        Courier.agent = [CourierAgent reactNativeIOS:@"5.6.11"];
         
         // Register for remote notifications
         UIApplication *app = [UIApplication sharedApplication];
